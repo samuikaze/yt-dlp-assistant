55fcccb90ccedc90ee61859e7c8b6370cdf7a471
SamuiKaze <rr5145@gmail.com>
2023-12-19 04:10:35 +0800

    feat: 增加儲存設定檔與多國語言功能
    調整項目:
        1. 當無設定檔存在時，會自動進行設定並將植存進設定檔中。
        2. 支援多國語言。
    
    Issue #5

--- yt-dlp-download.bat
+++ yt-dlp-download.bat
@@ -9,9 +9,47 @@ rem Copyright (C) 2023 SamuiKaze
 
 chcp 65001 >nul 2>&1
 
+set "re_setting_config=false"
+
+call :readConfigs
+call :cleanScreen
 call :showLicenseInfo
 
+if "%re_setting_config%" equ "false" (
+    set /p "re_setting_config=%lang_reconfig_question% "
+)
+
 goto configureTool
+
+:readConfigs
+    rem Load configurations from file if it exists, skip else.
+    if exist .\configs.ini (
+        echo Loading configurations from file, please wait ...
+        for /f "delims== tokens=1,2" %%G in (configs.ini) do set %%G=%%H
+    ) else (
+        set "re_setting_config=y"
+        echo Configuration file not found, skipping.
+    )
+
+    rem Loading language file after configurations are loaded.
+    rem If configurations file not found, using en-US as fallback language.
+    echo Loading language file, please wait ...
+    if "%lang%" equ "" (
+        set "lang=en-US"
+    )
+
+    set "lang_file=.\i18n\%lang%.lang"
+
+    if exist %lang_file% (
+        for /f "delims== tokens=1,2" %%M in (%lang_file%) do set %%M=%%N
+    ) else (
+        echo Due to lannguage configuration is empty, using en-US as default language.
+        for /f "delims== tokens=1,2" %%M in (.\i18n\en-US.lang) do set %%M=%%N
+    )
+
+    echo Configurations are loaded.
+
+    goto :eof
 
 :showLicenseInfo
     echo +---------------------------------------------------------------------+
@@ -24,7 +62,7 @@ goto configureTool
     echo ^| License detail can be found in `LICENSE` file.                      ^|
     echo ^|                                                                     ^|
     echo +---------------------------------------------------------------------+
-    echo ^| Version 1.1.1                                                       ^|
+    echo ^| Version 1.2.0                                                       ^|
     echo +---------------------------------------------------------------------+
     echo.
     goto :eof
@@ -39,79 +77,78 @@ goto configureTool
     call :showMessage
 
     if "%update_option%" equ "yes" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     ) else if "%update_option%" equ "true" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     )  else if "%update_option%" equ "y" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     )  else if "%update_option%" equ "t" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     ) else if "%update_option%" equ "YES" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     ) else if "%update_option%" equ "TRUE" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     ) else if "%update_option%" equ "Y" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     ) else if "%update_option%" equ "T" (
-        echo Updating yt-dlp ...
-        echo Do not terminate the process or the yt-dlp may be deleted by itself.
+        echo %lang_updating%
+        echo %lang_update_warning%
         .\yt-dlp.exe -U
     )
 
     goto :eof
 
 :showMessage
-    echo ^|^| Dependencies Check
+    echo ^|^| %lang_dependency_check_title%
     echo +----------------------------------------------------------------------
     if "%ytdlp_exists%" equ "true" (
         if "%ffmpeg_exists%" equ "true" (
-            echo ^| [Green] Check all passed!
+            echo ^| %lang_dependency_check_green%
         )
 
         if "%ffmpeg_exists%" equ "false" (
-            echo ^| [Yellow] There are some warnings found!
+            echo ^| %lang_dependency_check_yellow%
         )
     )
     echo ^|
     if "%ffmpeg_exists%" equ "false" (
-        echo ^| ^|^| ? Warning: ffmpeg.exe not found
+        echo ^| ^|^| ? %lang_ffmpeg_missing_warning_title%
         echo ^| ^|^|-------------------------------------------------------------------
-        echo ^| ^|^| ffmpeg.exe not found!
-        echo ^| ^|^| FFmpeg is required to merge video and audio file.
-        echo ^| ^|^| Or you'll need to merge them by yourself.
+        echo ^| ^|^| %lang_ffmpeg_missing_warning_message_l1%
+        echo ^| ^|^| %lang_ffmpeg_missing_warning_message_l2%
+        echo ^| ^|^| %lang_ffmpeg_missing_warning_message_l3%
         echo ^|
     )
 
     if "%ytdlp_exists%" equ "false" (
-        echo ^| ^|^| ! Error: yt-dlp.exe not found
+        echo ^| ^|^| ! %lang_yt_dlp_missing_error_title%
         echo ^| ^|^| ------------------------------------------------------------------
-        echo ^| ^|^| yt-dlp.exe not found!
-        echo ^| ^|^| yt-dlp is required to download the video.
-        echo ^| ^|^| Please put this batch file in the folder where `yt-dlp.exe` is lo-
-        echo ^| ^|^| cated.
+        echo ^| ^|^| %lang_yt_dlp_missing_error_message_l1%
+        echo ^| ^|^| %lang_yt_dlp_missing_error_message_l2%
+        echo ^| ^|^| %lang_yt_dlp_missing_error_message_l3%
         echo ^| ^|^|
-        echo ^| ^|^| Press any key to exit the tool to fix the errors.
+        echo ^| ^|^| %lang_yt_dlp_missing_error_message_lend%
         echo +----------------------------------------------------------------------
 
         goto exitTool
     )
 
     if "%ffmpeg_exists%" equ "false" (
-        echo ^| You can ignore warnings anyway or terminate the process.
+        echo ^| %lang_dependency_check_yellow_ignore_tip%
     )
 
     echo +----------------------------------------------------------------------
@@ -139,12 +176,12 @@ goto configureTool
         call :cleanScreen
         call :showLicenseInfo
 
-        echo ! Error: Video URI is required
+        echo ! %lang_video_uri_check_error_title%
         echo -----------------------------------------------------------------------
-        echo Video URI can not be empty.
-        echo Please specify a video URI to download.
+        echo %lang_video_uri_check_error_message_l1%
+        echo %lang_video_uri_check_error_message_l2%
         echo.
-        echo Press any key to reconfigure the settings.
+        echo %lang_video_uri_check_error_message_lend%
         echo.
         echo.
 
@@ -162,54 +199,111 @@ goto configureTool
     call :showLicenseInfo
     call :showMessage
 
-    rem Specify download folder and video URI
-    rem Download folder default point to user's download folder if user is not specify a path.
-    echo ^> Setting download folder. Default is current user's download folder.
-    echo ^> You can use relative path or absolute path to specify where to save the file.
-    echo ^> If folder doesn't exist, it will be created automatically.
-    set "store_path=%userprofile%\Downloads"
-    set "update_option=n"
-    set "video_resolution=4320"
-    set /P "store_path=Download folder: (Press Enter to skip) "
-    echo.
-    echo ^> This tool is designed to support Youtube ONLY even if yt-dlp support more sites.
-    echo ^> But you can still give it a try.
-    set /P "target_uri=Input video URI: (Required) "
+    if "%re_setting_config%" equ "y" (
+        rem Set default value first
+        set "store_path=%userprofile%\Downloads"
+        set "update_option=y"
+        set "video_resolution=4320"
+        rem Setting display language
+        echo %lang_setting_language_help_l1%
+        echo %lang_setting_language_help_l2%
+        echo %lang_setting_language_help_l3%
+        echo ^> https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+        echo ^> https://zh.wikipedia.org/wiki/ISO_3166-1
+        echo %lang_setting_language_help_l4%
+        set /p "lang=%lang_setting_language_tip% "
+        echo.
+        rem Specify download folder and video URI
+        rem Download folder default point to user's download folder if user is not specify a path.
+        echo %lang_setting_download_folder_help_l1%
+        echo %lang_setting_download_folder_help_l2%
+        echo %lang_setting_download_folder_help_l3%
+        set /p "store_path=%lang_setting_download_folder_tip% "
+        echo "%store_path%"
+        echo.
+        rem Setting video resolution
+        echo %lang_setting_video_resolution_help_l1%
+        echo %lang_setting_video_resolution_help_l2%
+        echo %lang_setting_video_resolution_help_l3%
+        set /p "video_resolution=%lang_setting_video_resolution_tip% "
+        echo.
+        rem Setting custom format options
+        echo %lang_setting_custom_format_options_help_l1%
+        echo %lang_setting_custom_format_options_help_l2%
+        echo %lang_setting_custom_format_options_help_l3%
+        echo %lang_setting_custom_format_options_help_l4%
+        set /p "format_options=%lang_setting_custom_format_options_tip% "
+        echo "%format_options%"
+        echo.
+        rem Setting update option
+        echo %lang_setting_update_option_help_l1%
+        echo %lang_setting_update_option_help_l2%
+        echo %lang_setting_update_option_help_l3%
+        set /p "update_option=%lang_setting_update_option_tip% "
+        echo.
+    )
+
+    echo %lang_setting_target_uri_help_l1%
+    echo %lang_setting_target_uri_help_l2%
+    set /p "target_uri=%lang_setting_target_uri_tip% "
+
     call :checkVideoURIEmpty
-    echo.
-    echo ^> Select what resolution to download. Default resolution is 4K.
-    echo ^> yt-dlp will download less than or equal to what resolution is specified.
-    echo ^> Using 4320, 2140, 1080, ..., etc. to specify resolution.
-    set /P "video_resolution=Specify video resolution: (Default is 4K, press Enter to skip) "
-    echo.
-    echo ^> You can use your format options here or just press enter to use default setting.
-    echo ^> Video resolution  is required if you change format options here.
-    echo ^> See official documents for more informations.
-    echo ^> You can press Enter to skip and use default format options.
-    set "format_options=bestvideo[height^<^=%video_resolution%][ext^=mp4]+bestaudio[ext^=m4a]/best[ext^=mp4]/best"
-    set /P "format_options=Input your format options: (Press Enter to skip) "
-    echo.
-    echo ^> Setting the process to update yt-dlp first or not, Default is `no`
-    echo ^> Download process will still continue regardless of update process is failed or not
-    echo ^> Update yt-dlp before download is strongly recommanded.
-    set /P "update_option=Update yt-dlp first? (Y/N) "
+
+    if "%re_setting_config%" equ "y" (
+        call :exportSettings
+        set "re_setting_config=false"
+    )
+
     goto confirmSettings
+
+:exportSettings
+    call :cleanScreen
+    call :showLicenseInfo
+    call :showMessage
+
+    echo %lang_exporting_settings_tip%
+
+    if "%format_options%" equ "" (
+        set "format_options=bestvideo[height^<^=%video_resolution%][ext^=mp4]+bestaudio[ext^=m4a]/best[ext^=mp4]/best"
+    )
+
+    (
+        echo lang=%lang%
+        echo store_path=%store_path%
+        echo video_resolution=%video_resolution%
+        echo update_option=%update_option%
+        echo %lang_exporting_settings_file_comment_custom_format_option%
+        echo ^; _format_options=
+    ) > configs.ini
+
+    echo %lang_apply_config_now_l1%
+    echo %lang_apply_config_now_l2%
+    call :readConfigs
+    goto :eof
 
 :confirmSettings
     call :cleanScreen
     call :showLicenseInfo
     call :showMessage
 
-    echo ^|^| Confirm Configurations
+    if "%format_options%" equ "" (
+        set "format_options=bestvideo[height^<^=%video_resolution%][ext^=mp4]+bestaudio[ext^=m4a]/best[ext^=mp4]/best"
+    )
+
+    echo ^|^| %lang_confirm_setting_title%
     echo +----------------------------------------------------------------------
-    echo ^| Download folder: %store_path%
-    echo ^| From URI: %target_uri%
-    echo ^| Format Options: %format_options%
-    echo ^| Update yt-dlp First: %update_option%
+    echo ^| %lang_confirm_setting_download_folder%: %store_path%
+    echo ^| %lang_confirm_setting_from_uri%: %target_uri%
+    echo ^| %lang_confirm_setting_format_options%: %format_options%
+    echo ^| %lang_confirm_setting_update_option%: %update_option%
     echo +----------------------------------------------------------------------
     echo.
+    echo %lang_confirm_setting_export_warning_l1%
+    echo %lang_confirm_setting_export_warning_l2%
+    echo %lang_confirm_setting_export_warning_l3%
+    echo.
     set "confirm_settings=Y"
-    set /P "confirm_settings=Confirm the configurations (Press `Enter` or `Y` to confirm, `N` to reconfigure) "
+    set /p "confirm_settings=%lang_confirm_setting_tip% "
     if "%confirm_settings%" equ "Y" (
         call :updateYTDLP
         goto mainDownloadProcess
@@ -223,6 +317,7 @@ goto configureTool
         call :updateYTDLP
         goto mainDownloadProcess
     ) else (
+        set /p "re_setting_config=%lang_reconfig_question% "
         goto configureTool
     )
 
@@ -231,17 +326,17 @@ goto configureTool
     call :showLicenseInfo
     call :showMessage
 
-    echo ^|^| Configurations
+    echo ^|^| %lang_download_process_config_title%
     echo +----------------------------------------------------------------------
-    echo ^| Download folder: %store_path%
-    echo ^| From URI: %target_uri%
-    echo ^| Format Options: %format_options%
+    echo ^| %lang_download_process_config_download_folder%: %store_path%
+    echo ^| %lang_download_process_config_from_uri%: %target_uri%
+    echo ^| %lang_download_process_config_format_options%: %format_options%
     echo +----------------------------------------------------------------------
     echo.
-    echo Begin download process with yt-dlp ...
-    echo You can terminate the process by pressing Ctrl + C
+    echo %lang_download_process_tip_l1%
+    echo %lang_download_process_tip_l2%
     echo.
-    echo Please note the download process will be ignored if the video had been downloaded before.
+    echo %lang_download_process_tip_lend%
     echo -----------------------------------------------------------------------
     echo.
 
